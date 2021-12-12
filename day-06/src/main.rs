b17f31b4142dac039c141b041d0b7c0e919a7376
Thomas Lent <tcl293@gmail.com>
2021-12-12 11:55:42 -0500

    Day 6

---
+++ day-06/src/main.rs
@@ -0,0 +1,57 @@
+const INPUT: &str = include_str!("../input");
+
+fn main() {
+    let initial_fish: Vec<u8> = INPUT
+        .trim()
+        .split(',')
+        .map(|s| s.parse().unwrap())
+        .collect();
+    println!("{}", part_one(&initial_fish));
+    println!("{}", part_two(&initial_fish));
+}
+
+fn part_one(fish: &[u8]) -> u64 {
+    fish_spawn_dynamic_programming(fish, 80)
+}
+
+fn part_two(fish: &[u8]) -> u64 {
+    fish_spawn_dynamic_programming(fish, 256)
+}
+
+fn fish_spawn_dynamic_programming(fish: &[u8], days: usize) -> u64 {
+    let mut fish_counts = vec![0; days + 1];
+    let mut spawn_counts = vec![0; days + 1];
+    fish_counts[0] = fish.len() as u64;
+    for &f in fish {
+        for day in (f as usize..=days).step_by(7) {
+            spawn_counts[day] += 1;
+        }
+    }
+    for current_day in 1..=days {
+        fish_counts[current_day] = fish_counts[current_day - 1] + spawn_counts[current_day - 1];
+        for spawn_day in ((current_day + 8)..=days).step_by(7) {
+            spawn_counts[spawn_day] += spawn_counts[current_day - 1];
+        }
+    }
+    fish_counts[days]
+}
+
+// initial solution that works for part one but is too slow for part two
+fn fish_spawn_sim(fish: &[u8], days: usize) -> u64 {
+    let mut fish = fish.to_vec();
+    for _ in 0..days {
+        let mut new_fish_count = 0;
+        for f in fish.iter_mut() {
+            if *f == 0 {
+                new_fish_count += 1;
+                *f = 6;
+            } else {
+                *f -= 1;
+            }
+        }
+        for _ in 0..new_fish_count {
+            fish.push(8);
+        }
+    }
+    fish.len() as u64
+}
