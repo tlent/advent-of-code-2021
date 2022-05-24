868e82f20bbd6656e6d37d9ebd7b3acb76656986
Thomas Lent <tcl293@gmail.com>
2022-05-24 14:24:06 -0400

    Day 14

--- day-14/src/main.rs
+++ day-14/src/main.rs
@@ -4,60 +4,68 @@ fn main() {
     let mut input_lines = INPUT.lines();
     let polymer_template = String::from(input_lines.next().unwrap());
     input_lines.next();
-    let mut pair_insertion_rules: [[Option<char>; 26]; 26] = [[None; 26]; 26];
+    let mut insertion_rules: PairMap<char> = Default::default();
     for line in input_lines {
         let mut parts = line.split(" -> ");
         let mut pair_chars = parts.next().unwrap().chars();
-        let first_pair_char_index = char_to_index(pair_chars.next().unwrap());
-        let second_pair_char_index = char_to_index(pair_chars.next().unwrap());
+        let pair = (pair_chars.next().unwrap(), pair_chars.next().unwrap());
         let element = parts.next().unwrap().chars().next().unwrap();
-        pair_insertion_rules[first_pair_char_index][second_pair_char_index] = Some(element);
+        *insertion_rules.get_mut(&pair) = element;
     }
-    println!("{}", part_one(&polymer_template, &pair_insertion_rules));
-    println!("{}", part_two(&polymer_template, &pair_insertion_rules));
+    let mut char_counts = [0; 26];
+    for c in polymer_template.chars() {
+        char_counts[char_to_index(c)] += 1;
+    }
+    let mut pair_counts: PairMap<u64> = Default::default();
+    let polymer_chars: Vec<_> = polymer_template.chars().collect();
+    for pair in polymer_chars.windows(2) {
+        *pair_counts.get_mut(&(pair[0], pair[1])) += 1;
+    }
+    for step in 1..=40 {
+        let prev_pair_counts = pair_counts.clone();
+        for a in 'A'..='Z' {
+            for b in 'A'..='Z' {
+                let count = *prev_pair_counts.get(&(a, b));
+                if count > 0 {
+                    let new = *insertion_rules.get(&(a, b));
+                    *pair_counts.get_mut(&(a, b)) -= count;
+                    *pair_counts.get_mut(&(a, new)) += count;
+                    *pair_counts.get_mut(&(new, b)) += count;
+                    char_counts[char_to_index(new)] += count;
+                }
+            }
+        }
+        if step == 10 {
+            println!(
+                "{}",
+                char_counts.iter().max().unwrap()
+                    - char_counts.iter().filter(|&&c| c > 0).min().unwrap()
+            )
+        }
+    }
+    println!(
+        "{}",
+        char_counts.iter().max().unwrap() - char_counts.iter().filter(|&&c| c > 0).min().unwrap()
+    )
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+struct PairMap<T>([[T; 26]; 26]);
+
+impl<T> PairMap<T> {
+    pub fn get(&self, pair: &(char, char)) -> &T {
+        let i = char_to_index(pair.0);
+        let j = char_to_index(pair.1);
+        &self.0[i][j]
+    }
+
+    pub fn get_mut(&mut self, pair: &(char, char)) -> &mut T {
+        let i = char_to_index(pair.0);
+        let j = char_to_index(pair.1);
+        &mut self.0[i][j]
+    }
 }
 
 fn char_to_index(c: char) -> usize {
     (c as u32 - 'A' as u32) as usize
-}
-
-fn part_one(polymer_template: &str, pair_insertion_rules: &[[Option<char>; 26]; 26]) -> u64 {
-    solve(polymer_template, pair_insertion_rules, 10)
-}
-
-fn part_two(polymer_template: &str, pair_insertion_rules: &[[Option<char>; 26]; 26]) -> u64 {
-    solve(polymer_template, pair_insertion_rules, 40)
-}
-
-fn solve(
-    polymer_template: &str,
-    pair_insertion_rules: &[[Option<char>; 26]; 26],
-    step_count: usize,
-) -> u64 {
-    let mut char_counts = [0; 26];
-    let polymer_chars: Vec<_> = polymer_template.chars().collect();
-    for pair in polymer_chars.windows(2) {
-        let mut polymer = String::from_iter(pair);
-        for _step in 1..=step_count {
-            let mut new_polymer = String::new();
-            let polymer_chars: Vec<_> = polymer.chars().collect();
-            for pair in polymer_chars.windows(2) {
-                let first_char_index = char_to_index(pair[0]);
-                let second_char_index = char_to_index(pair[1]);
-                let new_c = pair_insertion_rules[first_char_index][second_char_index].unwrap();
-                new_polymer.push(pair[0]);
-                new_polymer.push(new_c);
-            }
-            new_polymer.push(pair[1]);
-            polymer = new_polymer;
-        }
-        for c in polymer[..polymer.len() - 1].chars() {
-            char_counts[char_to_index(c)] += 1;
-        }
-    }
-    let c = polymer_chars[polymer_chars.len() - 1];
-    char_counts[char_to_index(c)] += 1;
-    let max_count = char_counts.iter().max().unwrap();
-    let min_count = char_counts.iter().filter(|&&n| n > 0).min().unwrap();
-    max_count - min_count
 }
