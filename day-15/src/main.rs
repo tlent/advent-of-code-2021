8b455e836704064d93a3b05d10e856daa5f85ffc
Thomas Lent <tcl293@gmail.com>
2022-05-24 23:54:33 -0400

    Day 15 with std heap

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -1,3 +1,6 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+
 const INPUT: &str = include_str!("../input");
 
 fn main() {
@@ -27,8 +30,8 @@ fn solve(costs: &Vec<Vec<u32>>) -> u32 {
     let start = (0, 0);
     let end = (size - 1, size - 1);
     let mut lowest_costs = vec![vec![u32::MAX; size]; size];
-    let mut heap = BinaryHeap::default();
-    heap.insert((0, start));
+    let mut heap = MinHeap::default();
+    heap.push((0, start));
     lowest_costs[0][0] = 0;
     while let Some((cost, position)) = heap.pop() {
         let (x, y) = position;
@@ -55,7 +58,7 @@ fn solve(costs: &Vec<Vec<u32>>) -> u32 {
             let new_cost = cost + costs[n.1][n.0];
             if new_cost < lowest_costs[n.1][n.0] {
                 lowest_costs[n.1][n.0] = new_cost;
-                heap.insert((new_cost, n));
+                heap.push((new_cost, n));
             }
         }
     }
@@ -63,52 +66,14 @@ fn solve(costs: &Vec<Vec<u32>>) -> u32 {
 }
 
 #[derive(Debug, Default, Clone)]
-struct BinaryHeap<T: Ord>(Vec<T>);
+struct MinHeap<T: Ord>(BinaryHeap<Reverse<T>>);
 
-impl<T: Ord> BinaryHeap<T> {
-    pub fn insert(&mut self, item: T) {
-        self.0.push(item);
-        let mut current_i = self.0.len() - 1;
-        let mut parent_i = if current_i % 2 == 0 {
-            current_i / 2
-        } else {
-            current_i - 1
-        };
-        while self.0[current_i] < self.0[parent_i] {
-            self.0.swap(current_i, parent_i);
-            current_i = parent_i;
-            parent_i = if current_i % 2 == 0 {
-                current_i / 2
-            } else {
-                current_i - 1
-            };
-        }
+impl<T: Ord> MinHeap<T> {
+    pub fn push(&mut self, item: T) {
+        self.0.push(Reverse(item));
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        if self.0.len() == 0 {
-            return None;
-        }
-        let swap_i = self.0.len() - 1;
-        self.0.swap(0, swap_i);
-        let v = self.0.pop();
-        let mut current_i = 0;
-        loop {
-            let left_i = current_i * 2;
-            let right_i = current_i * 2 + 1;
-            let mut smallest_i = current_i;
-            if left_i < self.0.len() && self.0[left_i] < self.0[smallest_i] {
-                smallest_i = left_i;
-            }
-            if right_i < self.0.len() && self.0[right_i] < self.0[smallest_i] {
-                smallest_i = right_i;
-            }
-            if smallest_i == current_i {
-                break;
-            }
-            self.0.swap(current_i, smallest_i);
-            current_i = smallest_i;
-        }
-        v
+        self.0.pop().map(|i| i.0)
     }
 }
