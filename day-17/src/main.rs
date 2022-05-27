42082be6f8ccd8bbde1e09c76d2922c1bb2286fa
Thomas Lent <tcl293@gmail.com>
2022-05-26 23:31:34 -0400

    Day 17

--- day-17/src/main.rs
+++ day-17/src/main.rs
@@ -4,7 +4,9 @@ const INPUT: &str = include_str!("../input");
 
 fn main() {
     let bounds = get_bounds(INPUT);
-    println!("{}", part_one(bounds));
+    let (solutions, max_vy) = solve(bounds);
+    println!("{}", (max_vy * (max_vy + 1)) / 2);
+    println!("{}", solutions.len());
 }
 
 fn get_bounds(input: &str) -> (RangeInclusive<i32>, RangeInclusive<i32>) {
@@ -21,18 +23,31 @@ fn get_bounds(input: &str) -> (RangeInclusive<i32>, RangeInclusive<i32>) {
     (min_x..=max_x, min_y..=max_y)
 }
 
-fn part_one((_, y_range): (RangeInclusive<i32>, RangeInclusive<i32>)) -> i32 {
-    let mut max_vy = 0;
-    for initial_vy in 0..1000 {
-        let mut y = 0;
-        let mut vy = initial_vy;
-        while vy > 0 || y > *y_range.start() {
-            y += vy;
-            vy -= 1;
-        }
-        if y_range.contains(&y) {
-            max_vy = initial_vy;
+fn solve((x_range, y_range): (RangeInclusive<i32>, RangeInclusive<i32>)) -> (Vec<(i32, i32)>, i32) {
+    let mut solutions = vec![];
+    let mut max_vy = -1000;
+    for initial_vy in -1000..1000 {
+        for initial_vx in 0..=*x_range.end() {
+            let (mut x, mut y) = (0, 0);
+            let mut vy = initial_vy;
+            let mut vx = initial_vx;
+            for _step in 0.. {
+                x += vx;
+                y += vy;
+                vy -= 1;
+                if vx > 0 {
+                    vx -= 1;
+                }
+                if x_range.contains(&x) && y_range.contains(&y) {
+                    solutions.push((initial_vx, initial_vy));
+                    max_vy = std::cmp::max(max_vy, initial_vy);
+                    break;
+                }
+                if x > *x_range.end() || (vy < 0 && y < *y_range.start()) {
+                    break;
+                }
+            }
         }
     }
-    ((max_vy + 1) * max_vy) / 2
+    (solutions, max_vy)
 }
